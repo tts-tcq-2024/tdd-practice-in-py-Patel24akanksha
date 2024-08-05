801f9785921259a55866f2d4983898d4454be794
Patel24akanksha <86720877+Patel24akanksha@users.noreply.github.com>
2024-08-05 11:42:40 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -6,7 +6,7 @@ class TestStringCalculator(unittest.TestCase):
                 self.assertEqual(add(""), 0)
                 
         '''def test_expectZeroForSingleZero(self):
-                self.assertEqual(add("0"), 0)
+                self.assertEqual(add("0"), 1)
                 
         def test_expectSumForTwoNumberst(self):
                 self.assertEqual(add("1,2"), 3)
